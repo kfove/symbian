fa595b32047c67a4d11f3ec7d89ac98511514df9
kfove <kfove.liang@gmail.com>
2022-12-03 12:16:37 +0800

    up

--- static/js/symbian.js
+++ static/js/symbian.js
@@ -1,6 +1,13 @@
 function n() {
     alert("啥都没有")
 }
-let devices = document.getElementsByClassName("devices").mousemove = () => {
-    console.log("Hello");
+window.addEventListener("click", (e) => {
+    let overlib = document.querySelector(".overlib");
+    overlib.setAttribute("style", `height: 50px; width:50px; display: block; position: absolute; top: ${e.pageY - 50}px; left: ${e.pageX - 25}px;z-index:100; transition: all 0.3s;`);
+    setTimeout(() => { overlib.setAttribute("style", "display: none;transition: all 0.3s;"); }, 2000);
+
+
+})
+document.getElementById("devices_list").onmouseover = function (e) {
+    console.log(e);
 }
